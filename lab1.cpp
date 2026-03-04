01c8e3efb562ad393e81152430c33f6e74409345
darenkova <118684273+darenkova@users.noreply.github.com>
2026-03-04 13:02:17 +0300

    Add files via upload

---
+++ lab1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+using namespace std;
+
+void printnum(int n) {
+    for (size_t i = 0; i < 6; i++) {
+        cout << "поток: " << n << " " << i << "\n";
+        this_thread::sleep_for(chrono::milliseconds(300));
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "");
+    
+    thread t1([]() { printnum(1); });
+    thread t2([]() { printnum(2); });
+    
+    for (size_t i = 0; i < 6; i++) {
+        cout << " Основной поток" << " " << i << "\n";
+        this_thread::sleep_for(chrono::milliseconds(400));
+    }
+    
+    t1.join();
+    t2.join();
+    return 0;
+}
